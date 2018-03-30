c3c8c02ac8ed2d6ebf4d2407e4e71a6e8a2f636d
何悦 <vipheyue@foxmail.com>
2018-03-30 20:46:47 +0800

    Initial commit

---
+++ checkrobot.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os
+import threading
+import time
+
+import requests
+import sys
+'''
+pip install requests
+'''
+
+
+def getInfo(processname):
+    task = os.popen('tasklist')
+    if processname in task.read():
+        print("-----ok")
+    else:
+        print("Sorry,the process is not in process list,may be not running......")
+        sendSms()
+        sys.exit(0)
+
+
+    print("-----return ---")
+
+
+# 每隔10秒钟执行
+def cycle():
+    while 1:
+        getInfo("robot.exe")
+        time.sleep(10)
+
+
+def sendSms():
+    url = 'http://m.vipss.info:9999/sendsms'
+    d = {'content': '机器人挂了', 'phone': '17623217250'}
+    r = requests.post(url, data=d)
+    print(r)
+
+
+if __name__ == '__main__':
+    t = threading.Thread(target=cycle)
+    t.start()
+    t.join()
